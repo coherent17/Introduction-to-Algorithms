b123500790f6d3db5b6ce9e0572b4cc7156121cf
coherent17 <mnb51817@gmail.com>
2022-10-26 04:10:06 +0800

    finish dp lack path

--- Lab1/0811562.cpp
+++ Lab1/0811562.cpp
@@ -42,6 +42,10 @@ void printArray(int *R, int R_size){
     printf("\n");
 }
 
+int max(int a, int b){
+    return a > b ? a : b;
+}
+
 int main(int argc, char *argv[]){
 
     int N, A, B;
@@ -53,18 +57,67 @@ int main(int argc, char *argv[]){
 
     int *dp1 = (int *)malloc(sizeof(int) * N);
     int *dp2 = (int *)malloc(sizeof(int) * N);
+    int *path1 = (int *)malloc(sizeof(int) * N);
+    int *path2 = (int *)malloc(sizeof(int) * N);
+    int *path = NULL;
 
     int no_resting_day = 1;
 
     //base case:
-    dp1[0] = A - B;
+    dp1[0] = A - 1 * 1 * B;
     dp2[0] = 0 - R[0];
 
-    //dp
+    // dp1[1] = max(dp1[0] + A - 2 * 2 * B, dp2[0] + A - B);   //right
+    // dp2[1] = max(dp1[0] - R[1], dp2[0] - R[1]);             //left
+
+    // dp1[2] = max(dp1[1] + A - 2 * 2 * B, dp2[1] + A - B);   //right
+    // dp2[2] = max(dp1[1] - R[2], dp2[1] - R[2]);             //right
+
+
+    
+
     for(int i = 1; i < N; i++){
-        if(dp1[i] + A - (no_resting_day + 1) * (no_resting_day + 1) * B  >= dp2[i]  )
+        if(dp1[i - 1] + A - (no_resting_day + 1) * (no_resting_day + 1) * B  >= dp2[i - 1] + A - B){
+            dp1[i] = dp1[i - 1] + A - (no_resting_day + 1) * (no_resting_day + 1) * B;
+            no_resting_day++;
+            path1[i] = 1;
+        }
+        else{
+            dp1[i] = dp2[i - 1] + A - B;
+            no_resting_day = 1;
+            path1[i] = 0;
+        }
+
+        if(dp1[i - 1] - R[i] >= dp2[i - 1] - R[i]){
+            dp2[i] = dp1[i - 1] - R[i];
+            path2[i] = 1;
+        }
+        else{
+            dp2[i] = dp2[i - 1] - R[i];
+            path2[i] = 0;
+        }
     }
 
+    // int temp;
+    // if(dp1[N-1] <= dp2[N-1]){
+    //     path = path2;
+    //     temp = 0;
+    // }
+    // else{
+    //     path = path1;
+    //     temp = 1;
+    // }
+
+    // printf("line %d, N=N-1\n", temp);
+    // for(int j = N-1; j >= 1; j--){
+    //     i = path
+    //     printf("%d -> ", path[j]);
+    // }
+
+    //printArray(dp1, N);
+    //printArray(dp2, N);
+    printf("%d\n", dp1[N-1]);
+    printf("%d\n", dp2[N-1]);
     free(R);
     return 0;
 }
