a09851c623290899dc6156eeb78e12e1aa8d9724
coherent17 <mnb51817@gmail.com>
2022-11-10 22:36:00 +0800

    testcase

---
+++ Lab1/Testcase/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CPP=$1
+g++ -std=c++11 ${CPP} -o a.out
+for i in {1..7};do
+    echo "case ${i} testing"
+    time ./a.out case${i}.txt out${i}
+    differ=$(diff -b out${i} golden_case${i}.txt | wc -w)
+    if [ ${differ} -eq 0 ];
+    then
+        echo "case${i} pass"
+        rm out${i}
+    else
+        diff -b out${i} golden_case${i}.txt
+    fi
+done
+
