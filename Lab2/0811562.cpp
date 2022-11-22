051cc654d645ea72776fdf447e968e87f74c9384
coherent17 <mnb51817@gmail.com>
2022-11-23 01:28:13 +0800

    update lab2

--- Lab2/0811562.cpp
+++ Lab2/0811562.cpp
@@ -1,18 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
-#define INT_MAX 2147483647
+using namespace std;
 
 typedef struct _point{
     int x;
     int y;
 }point;
 
+typedef struct _element{
+    int weight;
+    int x;
+    int y;
+}element;
+
 int abs(int x){
     return x >= 0 ? x : -x;
 }
 
-int **parser(char *filename, int *nptr, point **points){
+int find(vector<int> &ds, int i) {
+    return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
+}
+
+struct larger{
+    bool operator()(element& a, element& b) const{
+        return (a.weight > b.weight);
+    }
+};
+
+void parser(char *filename, int *nptr, point **points){
     FILE *input = fopen(filename, "r");
     fscanf(input, "%d", &(*nptr));
     *points = (point *)malloc(sizeof(point) * (*nptr));
@@ -20,32 +39,49 @@ int **parser(char *filename, int *nptr, point **points){
         fscanf(input, "%d %d", &((*points)[i].x), &((*points)[i].y));
     }
 
-    //build the adjacency matrix
-    int **matrix = (int **)malloc(sizeof(int *) * (*nptr));
-    for(int i = 0; i < (*nptr); i++){
-        matrix[i] = (int *)malloc(sizeof(int) * (*nptr));
-    }
+    int res = 0;
+    vector<int> ds(*nptr, -1);
+    vector<element> elements;
 
-    //init the matrix:
-    for(int i = 0; i < (*nptr); i++){
-        for(int j = 0; j < (*nptr); j++){
-            matrix[i][j] = INT_MAX;
+    //calculate the manhattan disatnce for each node
+    for (int i = 0; i < *nptr; i++){
+        for (int j = i + 1; j < *nptr; j++) {
+            elements.push_back({abs((*points)[i].x - (*points)[j].x) + abs((*points)[i].y - (*points)[j].y), i, j});
         }
     }
 
-    for(int i = 0; i < *nptr; i++){
-        for(int j = i + 1; j < *nptr; j++){
-            matrix[i][j] = abs((*points)[i].x - (*points)[j].x) + abs((*points)[i].y - (*points)[j].y);
+    //for those originally exist routing segment, set the weight to 0
+    int i, j;
+    while(fscanf(input, "%d %d", &i, &j) != EOF){
+        elements.push_back({0, i, j});
+    }
+    
+
+    make_heap(elements.begin(), elements.end(), larger());
+
+    for(int i = 0; i < elements.size(); i++){
+        cout << elements[i].weight << endl;
+    }
+
+    while (!elements.empty()) {
+        pop_heap(begin(elements), end(elements), larger());
+        auto ele = elements.back();
+        int dist = ele.weight;
+        int i = ele.x;
+        int j = ele.y;
+        elements.pop_back();
+        i = find(ds, i), j = find(ds, j);
+        if (i != j) {
+            res += dist;
+            ds[i] += ds[j];
+            ds[j] = i;
+            if (ds[i] == -(*nptr))
+                break;
         }
     }
 
-    int x, y;
-    while(fscanf(input, "%d %d", &x, &y) != EOF){
-        matrix[x][y] = 0;
-    }
-
+    printf("res = %d\n", res);
     fclose(input);
-    return matrix;
 }
 
 
@@ -54,28 +90,7 @@ int **parser(char *filename, int *nptr, point **points){
 int main(int argc, char *argv[]){
     int n;
     point *points;
-    int **matrix;
-    matrix = parser(argv[1], &n, &points);
-
-    printf("%d\n", n);
-    for(int i = 0; i < n; i++){
-        printf("%d %d\n", points[i].x, points[i].y);
-    }
-
-    printf("-----------------------------\n");
-    
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-
-
+    parser(argv[1], &n, &points);
     free(points);
-    for(int i = 0; i < n; i++){
-        free(matrix[i]);
-    }
-    free(matrix);
     return 0;
 }
